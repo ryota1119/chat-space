04c6c1f6943c251b7da2ab7f59d70ea4ebeb4966
ryota1119 <ibaryo.ngok.sxs@gmail.com>
2020-06-09 19:29:39 +0900

    Edit side_bar groups

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "groups#index"
   resources :users, only: [:edit, :update, :destroy]
-  resources :groups, only: [:new, :create, :edit, :update]
+  resources :groups, only: [:index, :new, :create, :edit, :update]
 end
