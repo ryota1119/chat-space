b7a8a2a1599f53cc1de3e8389bd0fa4e081de9cf
ryota1119 <ibaryo.ngok.sxs@gmail.com>
2020-06-27 18:59:01 +0900

    テンプレートリテラルを使ってjsonファイルのhtml作成

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,45 @@
 $(function() {
+
+  function buildHTML(message){
+    if ( message.image ) {
+      var html = `<div class="message">
+                    <div class="message__info">
+                      <div class="message__info__user-name">
+                        ${message.user_name}
+                      </div>
+                      <div class="message__info__time">
+                        ${message.created_at}
+                      </div>
+                    </div>
+                    <div class="message__text">
+                      <p class="message__text__content">
+                        ${message.content}
+                      </p>
+                    </div>
+                    <img src=${message.image}>
+                  </div>`
+      return html;
+    } else {
+      var html = `<div class="message">
+                    <div class="message__info">
+                      <div class="message__info__user-name">
+                        ${message.user_name}
+                      </div>
+                      <div class="message__info__time">
+                        ${message.created_at}
+                      </div>
+                    </div>
+                    <div class="message__text">
+                      <p class="message__text__content">
+                        ${message.content}
+                      </p>
+                    </div>
+                  </div>`
+      return html;
+    };
+  }
   
-  $('#new_message').on('submit', function(e){
+  $('#new_message').on('submit', function(e) {
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action');
@@ -12,5 +51,13 @@ $(function() {
       processData: false,
       contentType: false
     })
-  });
+    .done(function(data) {
+      var html = buildHTML(data);
+      $('.message-list').append(html);
+      $('form')[0].reset();
+    })
+    .always(() => {
+      $(".input-box__submit").removeAttr("disabled");
+      });
+  })
 });
