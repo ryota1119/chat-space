414792a47245201f8bf86769157fae1c82cce970
ryota1119 <ibaryo.ngok.sxs@gmail.com>
2020-06-28 17:31:47 +0900

    edit fails

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,18 @@
+$(function() {
+
+  $("#user-search-field").on('keyup', function() {
+    var input = $("#user-search-field").val();
+    $.ajax({
+      type: "GET",
+      url:  "/users",
+      data: { keyword: input },
+      dataType: "json"
+    })
+    .done(function(users) {
+      console.log("成功")
+    })
+    .fail(function() {
+      console.log("失敗");
+    });
+  });
+});
