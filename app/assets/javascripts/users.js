5c2eac9e369394aa437ead749ca94fac19d7cedc
ryota1119 <ibaryo.ngok.sxs@gmail.com>
2020-06-29 20:30:56 +0900

    edit: uers.js
    追加btnを押すとユーザーが移動する

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -6,14 +6,25 @@ $(function() {
         <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
       </div>`
       $("#user-search-result").append(html);
-    }
+  }
     
-    function addNoUser() {
-      var html = `
-      <div class="chat-group-user clearfix">
-        <p class="chat-group-user__name">ユーザーが見つかりません</p>
-      </div>`
-      $("#user-search-result").append(html);
+  function addNoUser() {
+    var html = `
+    <div class="chat-group-user clearfix">
+      <p class="chat-group-user__name">ユーザーが見つかりません</p>
+    </div>`
+    $("#user-search-result").append(html);
+  }
+  
+  function deleteUser(userName, userId) {
+    var html = `
+    <div class='chat-group-user'>
+            <input name='group[user_ids][]' type='hidden' value='ユーザーのid'>
+            <p class='chat-group-user__name'>${userName}</p>
+            <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
+          </div>
+          `
+  $("#chat-group-users").append(html)
   }
 
   $("#user-search-field").on('keyup', function() {
@@ -42,6 +53,10 @@ $(function() {
   });
 
   $("#user-search-result").on('click', ".chat-group-user__btn--add", function() {
-    console.log("確認")
+    var userId = $(this).data('user-id');
+    var userName = $(this).data('user-name');
+    $(this).parent().remove();
+    
+    deleteUser(userName, userId);
   });
 });
